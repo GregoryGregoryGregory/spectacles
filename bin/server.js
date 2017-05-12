34ac30351812c1d60fde092b12cd0dee1c7f3a65
Will Nelson <nelson.will@live.com>
2017-05-12 01:17:38 -0700

    try linking

--- bin/server.js
+++ bin/server.js
@@ -10,6 +10,6 @@ const server = http.createServer(app);
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(api(server));
+app.use(api(server), { host: 'redis' });
 
 server.listen(8080, () => console.log('listening on port 8080')); // eslint-disable-line no-console
